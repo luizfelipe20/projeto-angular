0ee8417b1383529f7f9c705fd100cc1f5143ddc6
luizfelipe20 <felipekjs36@gmail.com>
2020-09-15 09:13:12 -0300

    tipando a api

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Photo } from './photo';
 
 
 @Injectable(
@@ -15,6 +16,6 @@ export class PhotoService {
 
     listFromUser() {
         return this.http
-            .get<Object[]>(this.baseUrl + '/photos');
+            .get<Photo[]>(this.baseUrl + '/photos');
     }
 }
