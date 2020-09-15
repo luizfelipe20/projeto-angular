810e76058347af4bf8e501168cd07fe6544699d4
luizfelipe20 <felipekjs36@gmail.com>
2020-09-15 09:49:28 -0300

    organizando a aplicacao

--- src/app/photos/photo.module.ts
+++ src/app/photos/photo.module.ts
@@ -1,9 +1,14 @@
 import { PhotoComponent } from './photo/photo.component';
 import { NgModule } from '@angular/core';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [ PhotoComponent, PhotoListComponent ],
-    exports: [ PhotoComponent ]
+    imports: [
+        HttpClientModule,
+        CommonModule
+    ]
 })
 export class PhotoModule {}
