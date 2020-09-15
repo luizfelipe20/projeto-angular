a8b3c59ac646f5239347eb859f072c9f29f08dde
luizfelipe20 <felipekjs36@gmail.com>
2020-09-14 21:09:55 -0300

    criando componente de foto

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-photo',
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponent {
+
+    description =  'photo';
+    url = 'https://i.pinimg.com/originals/de/f6/96/def69643889ee29e232637646e839064.jpg';  
+
+}
