1e582db28219c2521ed7f088ff2078726470156b
luizfelipe20 <felipekjs36@gmail.com>
2020-09-15 22:19:31 -0300

    separando os componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 
-const routes: Routes = [
-    
-    // Exemplo de seguementos de rota
-    // { path: 'user/:userName', component: PhotoListComponent },
-    
+// Exemplo de seguementos de rota
+// { path: 'user/:userName', component: PhotoListComponent },
+
+const routes: Routes = [  
     { path: 'photos', component: PhotoListComponent },
-    // { path: 'photo/add', component: PhotoFormComponent },
+    { path: 'photo/add', component: PhotoFormComponent },
     { path: '**', component: NotfoundComponent }
 ];
 
